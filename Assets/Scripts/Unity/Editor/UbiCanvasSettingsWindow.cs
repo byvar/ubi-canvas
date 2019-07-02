8d2dcbf5910b7d34b99731cd94c391311d3768ae
RayCarrot <RayCarrotMaster@gmail.com>
2019-07-02 20:33:42 +0200

    Added the UbiCanvas settings editor

---
+++ Assets/Scripts/Unity/Editor/UbiCanvasSettingsWindow.cs
@@ -0,0 +1,110 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using UbiArt;
+using UnityEditor;
+using Path = System.IO.Path;
+
+public class UbiCanvasSettingsWindow : EditorWindow
+{
+    #region Unity Methods
+
+    [MenuItem("Ubi-Canvas/Ubi-Canvas Setup")]
+    public static void ShowWindow()
+    {
+        GetWindow<UbiCanvasSettingsWindow>(false, "Ubi-Canvas Setup", true);
+    }
+
+    public void OnGUI()
+    {
+        // Increase label width due to it being cut off otherwise
+        EditorGUIUtility.labelWidth = 280;
+
+        // Update game mode first
+        UbiCanvasSettings.GameMode = (Settings.Mode)EditorGUILayout.EnumPopup("Game", UbiCanvasSettings.GameMode);
+
+        // If the current data directory has changed or no files were found then attempt to refresh the list of available levels
+        if (PreviousGameDataFolder != UbiCanvasSettings.CurrentGameDataDir || AvailableFiles == null)
+        {
+            AvailableFiles = FindLevelFiles().
+                // Replace the directory separator character with a forward slash so that Unity will split them into sub-menus in the popup
+                Select(x => x.Replace(Path.DirectorySeparatorChar, '/')).ToArray();
+
+            PreviousGameDataFolder = UbiCanvasSettings.CurrentGameDataDir;
+        }
+
+        // Update the data and create controls
+        UbiCanvasSettings.ROGameDataDir = EditorGUILayout.TextField("Rayman Origins data", UbiCanvasSettings.ROGameDataDir);
+        UbiCanvasSettings.RLGameDataDir = EditorGUILayout.TextField("Rayman Legends data", UbiCanvasSettings.RLGameDataDir);
+        UbiCanvasSettings.RLVitaCatchThemAllGameDataDir = EditorGUILayout.TextField("Rayman Legends PS Vita Catch Them All data", UbiCanvasSettings.RLVitaCatchThemAllGameDataDir);
+        UbiCanvasSettings.RAGameDataDir = EditorGUILayout.TextField("Rayman Adventures data", UbiCanvasSettings.RAGameDataDir);
+        SelectedLevelFileIndex = EditorGUILayout.Popup("Scene", SelectedLevelFileIndex, AvailableFiles);
+        UbiCanvasSettings.SelectedLevelFile = AvailableFiles.ElementAtOrDefault(SelectedLevelFileIndex);
+
+        // Buttons to manually save
+        //if (GUILayout.Button("Load"))
+        //    UbiCanvasSettings.Load();
+
+        //if (GUILayout.Button("Save"))
+        //    UbiCanvasSettings.Save();
+    }
+
+    void OnFocus()
+    {
+        UbiCanvasSettings.Load();
+    }
+
+    void OnLostFocus()
+    {
+        UbiCanvasSettings.Save();
+    }
+
+    void OnDestroy()
+    {
+        UbiCanvasSettings.Save();
+    }
+    #endregion
+
+    #region Private Methods
+
+    /// <summary>
+    /// Gets the relative path of all level files in the selected game data bin
+    /// </summary>
+    /// <returns>The relative paths</returns>
+    private List<string> FindLevelFiles()
+    {
+        // Create the output
+        var output = new List<string>();
+
+        // If the directory does not exist, return the empty list
+        if (!Directory.Exists(UbiCanvasSettings.CurrentGameDataDir))
+            return output;
+
+        // Add the found files containing the correct file extension
+        output.AddRange(from file in Directory.EnumerateFiles(UbiCanvasSettings.CurrentGameDataDir, "*.isc*", SearchOption.AllDirectories) select file.Remove(0, UbiCanvasSettings.CurrentGameDataDir.Length + 1));
+
+        // Return the output
+        return output;
+    }
+
+    #endregion
+
+    #region Properties
+
+    /// <summary>
+    /// The selected level file index, based on <see cref="AvailableFiles"/>
+    /// </summary>
+    private int SelectedLevelFileIndex { get; set; }
+
+    /// <summary>
+    /// The available level files for the selected game
+    /// </summary>
+    private string[] AvailableFiles { get; set; }
+
+    /// <summary>
+    /// Copy of the previous game data folder for reference
+    /// </summary>
+    private string PreviousGameDataFolder { get; set; }
+
+    #endregion
+}
---
+++ Assets/Scripts/Unity/UbiCanvasSettings.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using UbiArt;
+using UnityEditor;
+
+/// <summary>
+/// Settings for UbiCanvas
+/// </summary>
+public static class UbiCanvasSettings
+{
+    public static string ROGameDataDir { get; set; }
+
+    public static string RLGameDataDir { get; set; }
+
+    public static string RLVitaCatchThemAllGameDataDir { get; set; }
+
+    public static string RAGameDataDir { get; set; }
+
+    public static Settings.Mode GameMode { get; set; } = Settings.Mode.RaymanLegendsPC;
+
+    public static string CurrentGameDataDir
+    {
+        get
+        {
+            switch (GameMode)
+            {
+                case Settings.Mode.RaymanOriginsPC:
+                    return ROGameDataDir;
+
+                case Settings.Mode.RaymanLegendsPC:
+                    return RLGameDataDir;
+
+                case Settings.Mode.RaymanLegendsVitaCatchThemAll:
+                    return RLVitaCatchThemAllGameDataDir;
+
+                case Settings.Mode.RaymanAdventuresAndroid:
+                    return RAGameDataDir;
+
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(GameMode));
+            }
+        }
+    }
+
+    public static string SelectedLevelFile { get; set; }
+
+    /// <summary>
+    /// Saves the settings
+    /// </summary>
+    public static void Save()
+    {
+        EditorPrefs.SetString(nameof(ROGameDataDir), ROGameDataDir);
+        EditorPrefs.SetString(nameof(RLGameDataDir), RLGameDataDir);
+        EditorPrefs.SetString(nameof(RLVitaCatchThemAllGameDataDir), RLVitaCatchThemAllGameDataDir);
+        EditorPrefs.SetString(nameof(RAGameDataDir), RAGameDataDir);
+        EditorPrefs.SetString("UbiGameMode", GameMode.ToString());
+        EditorPrefs.SetString("SelectedUbiLevelFile", SelectedLevelFile);
+    }
+
+    /// <summary>
+    /// Loads the settings
+    /// </summary>
+    public static void Load()
+    {
+        ROGameDataDir = EditorPrefs.GetString(nameof(ROGameDataDir), ROGameDataDir);
+        RLGameDataDir = EditorPrefs.GetString(nameof(RLGameDataDir), RLGameDataDir);
+        RLVitaCatchThemAllGameDataDir = EditorPrefs.GetString(nameof(RLVitaCatchThemAllGameDataDir), RLVitaCatchThemAllGameDataDir);
+        RAGameDataDir = EditorPrefs.GetString(nameof(RAGameDataDir), RAGameDataDir);
+        GameMode = Enum.TryParse(EditorPrefs.GetString("UbiGameMode", GameMode.ToString()), out Settings.Mode gameMode) ? gameMode : GameMode;
+        SelectedLevelFile = EditorPrefs.GetString("SelectedUbiLevelFile", SelectedLevelFile);
+    }
+}
