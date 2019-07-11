763eb9166cf18851fb9740220b2529872fe9b02e
byvar <39142447+byvar@users.noreply.github.com>
2019-07-11 19:10:30 +0200

    Corrected localisation loading for Legends

--- Assets/Scripts/UbiArt/PureBinary/Localisation/LocAudio.cs
+++ Assets/Scripts/UbiArt/PureBinary/Localisation/LocAudio.cs
@@ -8,11 +8,21 @@ using UnityEngine;
 namespace UbiArt.Localisation {
 	// See: ITF::LocAudio::serialize
 	public class LocAudio : CSerializable {
+		[Serialize("unk")] public uint unk;
 		[Serialize("text")] public string text;
+		[Serialize("unk0")] public byte unk0;
+		[Serialize("unk1")] public byte unk1;
+		[Serialize("unk2")] public byte unk2;
+		[Serialize("unk3")] public byte unk3;
 
 		protected override void SerializeImpl(CSerializerObject s) {
 			base.SerializeImpl(s);
+			SerializeField(s, nameof(unk));
 			SerializeField(s, nameof(text));
+			SerializeField(s, nameof(unk0));
+			SerializeField(s, nameof(unk1));
+			SerializeField(s, nameof(unk2));
+			SerializeField(s, nameof(unk3));
 		}
 	}
 }
--- Assets/Scripts/UbiArt/PureBinary/Localisation/Localisation_Template.cs
+++ Assets/Scripts/UbiArt/PureBinary/Localisation/Localisation_Template.cs
@@ -12,14 +12,31 @@ namespace UbiArt.Localisation {
 		[Serialize("strings")] public CMap<int, CMap<LocalisationId, LocText>> strings;
 		[Serialize("audio")] public CMap<LocalisationId, LocAudio> audio;
 		[Serialize("paths")] public CList<Path> paths;
-		[Serialize("unk")] public CList<int> unk;
+		[Serialize("count")] public uint count;
+		public uint[] unk;
 
 		protected override void SerializeImpl(CSerializerObject s) {
 			base.SerializeImpl(s);
 			SerializeField(s, nameof(strings));
 			SerializeField(s, nameof(audio));
 			SerializeField(s, nameof(paths));
-			SerializeField(s, nameof(unk));
+			// Special array with sometimes predefined size
+			if (Settings.s.game == Settings.Game.RA) {
+				// Adventures: length specified in file, but when writing, the script always writes 25.
+				SerializeField(s, nameof(count));
+			} else {
+				// Legends: length not specified in file. When writing, the script always writes 19.
+				count = 19;
+			}
+			if (unk == null) {
+				unk = new uint[count];
+			}
+			for (int i = 0; i < count; i++) {
+				if (s.ArrayEntryStart(name: nameof(unk), index: i)) {
+					s.Serialize<uint>(ref unk[i], name: nameof(unk), index: i);
+					s.ArrayEntryStop();
+				}
+			}
 		}
 	}
 }
