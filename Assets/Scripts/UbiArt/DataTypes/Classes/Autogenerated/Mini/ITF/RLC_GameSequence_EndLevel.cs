f77341b860697029f85a4ac15369ce86ed46af6f
byvar <39142447+byvar@users.noreply.github.com>
2023-06-04 19:03:33 +0200

    Fix broken files

--- Assets/Scripts/UbiArt/DataTypes/Classes/Autogenerated/Mini/ITF/RLC_GameSequence_EndLevel.cs
+++ Assets/Scripts/UbiArt/DataTypes/Classes/Autogenerated/Mini/ITF/RLC_GameSequence_EndLevel.cs
@@ -1,10 +1,8 @@
 namespace UbiArt.ITF {
 	[Games(GameFlags.RM)]
 	public partial class RLC_GameSequence_EndLevel : CSerializable {
-		public Placeholder RLC_GameSequence_EndLevel;
 		protected override void SerializeImpl(CSerializerObject s) {
 			base.SerializeImpl(s);
-			RLC_GameSequence_EndLevel = s.SerializeObject<Placeholder>(RLC_GameSequence_EndLevel, name: "RLC_GameSequence_EndLevel");
 		}
 		public override uint? ClassCRC => 0xDA009D33;
 	}
--- Assets/Scripts/UbiArt/DataTypes/Classes/Autogenerated/Mini/ITF/RLC_GameSequence_StartLevel.cs
+++ Assets/Scripts/UbiArt/DataTypes/Classes/Autogenerated/Mini/ITF/RLC_GameSequence_StartLevel.cs
@@ -1,10 +1,8 @@
 namespace UbiArt.ITF {
 	[Games(GameFlags.RM)]
 	public partial class RLC_GameSequence_StartLevel : CSerializable {
-		public Placeholder RLC_GameSequence_StartLevel;
 		protected override void SerializeImpl(CSerializerObject s) {
 			base.SerializeImpl(s);
-			RLC_GameSequence_StartLevel = s.SerializeObject<Placeholder>(RLC_GameSequence_StartLevel, name: "RLC_GameSequence_StartLevel");
 		}
 		public override uint? ClassCRC => 0x372966BF;
 	}
