f21331a36f9a3313e972e3dcacb461b5fc4d185b
byvar <39142447+byvar@users.noreply.github.com>
2023-06-04 18:44:09 +0200

    2 more renamed classes... guess I'll automate this

--- Assets/Scripts/UbiArt/DataTypes/Classes/Autogenerated/Legends/ITF/VideoResource.cs
+++ Assets/Scripts/UbiArt/DataTypes/Classes/Autogenerated/Legends/ITF/VideoResource.cs
@@ -1,6 +1,6 @@
 namespace UbiArt.ITF {
 	[Games(GameFlags.RL | GameFlags.RM)]
-	public partial class Unknown_RL_291_sub_937AA0 : CSerializable {
+	public partial class VideoResource : CSerializable {
 		protected override void SerializeImpl(CSerializerObject s) {
 			base.SerializeImpl(s);
 		}
